91133eca8399afaec5be4eeb768ba0d4b8b5905f
doquangthinh99 <quangthinh123321@gmail.com>
2019-11-03 10:35:40 +0700

    Chỉnh sửa nội dung, routing /checkout

--- project/app.js
+++ project/app.js
@@ -6,6 +6,7 @@ var logger = require('morgan');
 
 var homeRouter = require('./routes/home');
 var usersRouter = require('./routes/users');
+var usersRouter = require('./routes/checkout');
 
 var app = express();
 
@@ -21,6 +22,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', homeRouter);
 app.use('/users', usersRouter);
+app.use('/checkout', checkoutRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
